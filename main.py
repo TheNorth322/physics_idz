592faa012f8eb04b7af1a5d0c473a0e4c7db2edc
TheNorth322 <lol_danchik@bk.ru>
2022-06-13 23:57:40 +0700

    added comments

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import *
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 
+# Теплоемкости металлов
 heat_capacities = {'Железо': 452,
             'Алюминий': 897,
             'Медь': 420,
@@ -17,18 +18,19 @@ def make_calc():
     x = []
     y = []
     
-    start_temperature = 400 # начальная температура шара
-    end_temperature = 300 # конечная температура шара
-    air_temperature = 280 # температура воздуха
+    start_temperature = 400
+    end_temperature = 300
+    air_temperature = 280
     
     try:
-        radius = float(radius_entry.get()) # радиус шара
+        radius = float(radius_entry.get())
     except:
         radius_entry.config(highlightthickness=2, highlightbackground="red")
         return
 
     try:
-        heat_transfer = float(heat_transfer_entry.get()) # коэффициент теплоотдачи
+        # Коэффициент теплоотдачи
+        heat_transfer = float(heat_transfer_entry.get())
     except:
         heat_transfer_entry.config(highlightthickness=2, highlightbackground="red")
         return
@@ -51,6 +53,7 @@ def make_calc():
     update_plot(x, y)
     result_entry.insert(0, "{:.2f}".format(x[0]))
 
+# Обновление графика
 def update_plot(x, y):
     global figure, plot, canvas
     figure.delaxes(plot)
@@ -68,7 +71,7 @@ def update_plot(x, y):
     toolbar.update()
     canvas.get_tk_widget().pack()
 
-#GUI
+# Графический пользовательский интерфейс
 window = Tk()
 window.title("Время остывание металлического шара")
 window.geometry("1000x600+560+200")
@@ -76,17 +79,20 @@ window.resizable(0,0)
 window.config(bg = 'white')
 
 plot_frame = Frame(window, bg="white")
-plot_frame.pack(side=LEFT) # for plot
+plot_frame.pack(side=LEFT)
 
 data_frame = Frame(window, bg="white")
-data_frame.pack(side=LEFT, fill=Y, padx = 10, pady = 10) # for data input
+data_frame.pack(side=LEFT, fill=Y, padx = 10, pady = 10) 
 
 Label(data_frame, text="Рассчет времени остывания\n шарика в воздухе", bg="white", font="Arial 15 bold").pack(side=TOP, anchor=N) 
+
+# Поле для ввода радиуса шарика
 Label(data_frame, text="Радиус шарика, м", bg="white", font="Arial 15 italic").pack(anchor = W)
 radius_entry = Entry(data_frame, font="Arial 15", borderwidth=3, relief= RIDGE, width=10)
 radius_entry.pack(pady=5, fill=X, padx=5, anchor = W)
 radius_entry.insert(0, 0.01)
 
+# Меню для выбора материала шарика
 Label(data_frame, text="Материал", bg="white", font="Arial 15 italic").pack(anchor = W)
 choice = StringVar()
 choice.set('Железо')
@@ -94,15 +100,18 @@ heat_capacities_list = OptionMenu(data_frame, choice, *heat_capacities)
 heat_capacities_list.config(bg="white", relief = RIDGE, font="Arial 15")
 heat_capacities_list.pack(pady=5, fill=X, padx=5, anchor = W)
 
+# Поле для ввода коэффициента теплоотдачи
 Label(data_frame, text="Коэффициент теплоотдачи", bg="white", font="Arial 15 italic").pack(anchor = W)
 heat_transfer_entry = Entry(data_frame, font="Arial 15", relief=RIDGE, borderwidth=3, width=10)
 heat_transfer_entry.pack(pady=5, fill=X, padx=5, anchor = W)
 heat_transfer_entry.insert(0, 0.01)
 
+# Поле для вывода результата
 Label(data_frame, text="Результат, с", bg="white", font="Arial 15 italic").pack(anchor = W)
 result_entry = Entry(data_frame, font="Arial 15", relief=RIDGE, borderwidth=3, width=10)
 result_entry.pack(pady=5, fill=X, padx=5, anchor = W)
 
+# Окно с графиком
 figure = Figure(figsize=(6, 5), dpi=110)
 plot = figure.add_subplot(111)
 plot.plot()
@@ -118,6 +127,7 @@ toolbar = NavigationToolbar2Tk(canvas, plot_frame)
 toolbar.update()
 canvas.get_tk_widget().pack()
 
+# Кнопка запуска программы
 start_button = Button(data_frame, text="Рассчитать время", bg="white", font="Arial 15 bold",relief=RIDGE, command=make_calc)
 start_button.pack(pady=5, fill=X, padx=5, side = BOTTOM, anchor=S)
 window.mainloop()
